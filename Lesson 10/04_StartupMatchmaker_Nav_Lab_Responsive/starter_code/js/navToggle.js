d4d11782b78cc2ba8aae1c5768b50a0b1d4657fb
mikodougherty <miko.dougherty@gmail.com>
2023-02-16 20:57:22 -0500

    classwork 02-16

---
+++ Lesson 10/04_StartupMatchmaker_Nav_Lab_Responsive/starter_code/js/navToggle.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function(event) {
+
+
+});
+
+document.addEventListener('click', function(){
+    document.querySelector(".main-nav").classList.toggle("hideMe");
+
+});
