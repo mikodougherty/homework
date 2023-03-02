fad5136d6eb2e83bb6ea55f6f752c9c9b877ab44
mikodougherty <miko.dougherty@gmail.com>
2023-03-01 21:30:41 -0500

    homework 0301

---
+++ Homework 06/starter_code/js/trulia.js
@@ -0,0 +1,33 @@
+
+
+let truliaCards = document.getElementsByClassName("trulia-grid-item");
+
+
+
+document.addEventListener('DOMContentLoaded', function(event) {
+
+// Toggle the navigation
+document.querySelector(".trulia-nav-toggle").addEventListener('click',function(e){
+  e.preventDefault();
+  document.querySelector(".trulia-nav-mobilehide").classList.toggle("trulia-nav-mobileshow");
+});
+
+
+
+
+
+// Loop through all the cards
+    
+  // Add a click listener on each card
+      
+      // Remove the featured class
+
+
+      // Add the featured class on the one clicked on
+
+
+
+  });  
+
+
+
